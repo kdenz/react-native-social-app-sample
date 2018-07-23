28fa3b0adbd5010a44ed39ac160cb3708888d710
kdenz <ksnz93@gmail.com>
2018-07-24 00:56:20 +0800

    Implement reusable screen container component

---
+++ src/components/ScreenContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Text, StyleSheet, ScrollView } from "react-native";
+import theme from "../styles/theme";
+
+export default class ScreenContainer extends Component {
+  render() {
+    const { title, children } = this.props;
+    return (
+      <ScrollView style={styles.container}>
+        {title && <Text style={styles.title}>{title}</Text>}
+        {children}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: theme.PRIMARY_COLOR,
+    marginTop: 10,
+    fontSize: theme.FONT_SIZE_XL,
+    fontWeight: "bold"
+  },
+  container: {
+    backgroundColor: theme.SECONDARY_COLOR,
+    padding: 20,
+    flex: 1
+  }
+});
