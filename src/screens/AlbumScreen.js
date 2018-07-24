6caad0788b0cac749cff1dbd2741a8aa63475a0b
kdenz <ksnz93@gmail.com>
2018-07-25 04:41:19 +0800

    Implement AlbumScreen

--- src/screens/AlbumScreen.js
+++ src/screens/AlbumScreen.js
@@ -1,12 +1,73 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, FlatList } from "react-native";
+import PhotoStore from "../stores/PhotoStore";
+import FriendStore from "../stores/FriendStore";
+import ScreenContainer from "../components/ScreenContainer";
+import { Subscribe } from "unstated";
+import Post from "../components/Post";
 
-export default class AlbumScreen extends Component {
+export default class AlbumScreenContainer extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <Subscribe to={[PhotoStore, FriendStore]}>
+        {(photoStore, friendStore) => (
+          <AlbumScreen photoStore={photoStore} friendStore={friendStore} />
+        )}
+      </Subscribe>
+    );
+  }
+}
+
+class AlbumScreen extends Component {
+  componentDidMount() {
+    const { photoStore } = this.props;
+    photoStore.initializePhotoStore();
+  }
+
+  renderItem = ({ item }) => {
+    const {
+      friendStore: {
+        state: { friendMap }
+      },
+      photoStore: {
+        state: { photoMap }
+      }
+    } = this.props;
+    const userItem = friendMap[item.userId];
+
+    let userName, userCompany, userCompanyCatchPhrase;
+    if (userItem) {
+      userName = userItem.username;
+      userCompany = userItem.company.name;
+      userCompanyCatchPhrase = userItem.company.catchPhrase;
+    }
+
+    //Returns first 4 photos to show as thumbnails
+    const photos = photoMap[item.id] && photoMap[item.id].slice(0, 4);
+
+    return (
+      <Post
+        albumId={item.id}
+        photos={photos}
+        title={item.title}
+        userName={userName}
+        userCompany={userCompany}
+        userCompanyCatchPhrase={userCompanyCatchPhrase}
+      />
+    );
+  };
+
+  render() {
+    const { photoStore } = this.props;
+    return (
+      <ScreenContainer title={"Albums"}>
+        <FlatList
+          data={photoStore.state.albums}
+          renderItem={this.renderItem}
+          style={{ marginTop: 10 }}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </ScreenContainer>
     );
   }
 }
