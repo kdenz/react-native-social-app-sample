419606f40aa1eaeefb3fba1d2a02293cd9a601b8
kdenz <ksnz93@gmail.com>
2018-07-25 05:24:18 +0800

    Implement FriendScreen with a list of user's friends

--- src/screens/FriendScreen.js
+++ src/screens/FriendScreen.js
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, FlatList } from "react-native";
+import ScreenContainer from "../components/ScreenContainer";
+import { Subscribe } from "unstated";
+import FriendStore from "../stores/FriendStore";
+import Todo from "../components/Todo";
+import UserBadge from "../components/UserBadge";
 
-export default class FriendScreen extends Component {
+export default class FriendScreenContainer extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <Subscribe to={[FriendStore]}>
+        {friendStore => <FriendScreen friendStore={friendStore} />}
+      </Subscribe>
+    );
+  }
+}
+
+class FriendScreen extends Component {
+  componentDidMount() {
+    const { friendStore } = this.props;
+  }
+  renderItem = ({ item }) => {
+    return (
+      <UserBadge
+        cardMode
+        userName={item.name}
+        userCompany={item.company.name}
+        userCompanyCatchPhrase={item.company.catchPhrase}
+      />
+    );
+  };
+
+  render() {
+    const { friendStore } = this.props;
+    return (
+      <ScreenContainer title={"My Friends"}>
+        <FlatList
+          data={friendStore.state.friends}
+          renderItem={this.renderItem}
+          style={{ marginTop: 10, paddingBottom: 100 }}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </ScreenContainer>
     );
   }
 }
