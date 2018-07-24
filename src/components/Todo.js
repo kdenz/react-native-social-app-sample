7601092d111977830718c0a338ac9bdc9f1074ad
kdenz <ksnz93@gmail.com>
2018-07-25 05:23:15 +0800

    Implement TaskScreen with reusable Todo.js component

---
+++ src/components/Todo.js
@@ -0,0 +1,44 @@
+import React, { PureComponent } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+export default class Todo extends PureComponent {
+  render() {
+    const { done, title } = this.props;
+    return (
+      <View style={[styles.container, done ? styles.doneStyle : {}]}>
+        <Text style={styles.title}> {title} </Text>
+        <Icon
+          name={"check"}
+          color={done ? "#2ECC40" : "white"}
+          size={40}
+          style={{ marginTop: 5 }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+
+    shadowOffset: { width: 1, height: 2 },
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    elevation: 3,
+    backgroundColor: "white",
+    borderRadius: 10,
+    marginBottom: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    margin: 5
+  },
+  doneStyle: {
+    backgroundColor: "#01FF70"
+  },
+  title: {
+    flex: 1
+  }
+});
--- src/screens/TaskScreen.js
+++ src/screens/TaskScreen.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, FlatList } from "react-native";
+import ScreenContainer from "../components/ScreenContainer";
+import { Subscribe } from "unstated";
+import TaskStore from "../stores/TaskStore";
+import Todo from "../components/Todo";
 
-export default class TaskScreen extends Component {
+export default class TaskScreenContainer extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <Subscribe to={[TaskStore]}>
+        {taskStore => <TaskScreen taskStore={taskStore} />}
+      </Subscribe>
+    );
+  }
+}
+
+class TaskScreen extends Component {
+  componentDidMount() {
+    const { taskStore } = this.props;
+    taskStore.loadTodoList();
+  }
+  renderItem = ({ item }) => {
+    return <Todo title={item.title} done={item.completed} />;
+  };
+
+  render() {
+    const { taskStore } = this.props;
+    return (
+      <ScreenContainer title={"My Tasks"}>
+        <FlatList
+          data={taskStore.state.tasks}
+          renderItem={this.renderItem}
+          style={{ marginTop: 10 }}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </ScreenContainer>
     );
   }
 }
